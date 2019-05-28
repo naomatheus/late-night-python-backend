eb02ddcabfc8cbb4b775c09d2e685331e03c42d0
matthew Robinson <matthewrobinson@matthews-MacBook-Air-3.local>
2019-05-28 11:36:52 -0500

    setting up resources, checked models and table structure with reuben

--- models.py
+++ models.py
@@ -2,17 +2,41 @@ import datetime
 
 from peewee import *
 
-DATABASE = 
+DATABASE = SqliteDatabase('late-night.sqlite')
 
-class Name(Model):
+# eventually we'll switch to POSTGRES and uncomment this
+
+# DATABASE = PostgresqlDatabase('late-night', user='matton',password='matton')
+
+
+
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
+
+class User(Model):
 	name=CharField()
 
-
-
+# DO NOT NEED A COMMENTS MADE OR COMMENT ID HERE
 
 	class Meta:
 		database=DATABASE
 
+
+class Place(Model):
+	# name=CharField()
+
+# DO NOT NEED A COMMENT ID or comment key in this table
+
+	class Meta:
+		database=DATABASE
+
+
+class Comment(Model):
+	# name=CharField()
+
+
+	class Meta:
+		database=DATABASE
 
 # def initialize():
 # 	DATABASE.connect()
