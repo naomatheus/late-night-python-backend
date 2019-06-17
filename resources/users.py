86a8c19e3b688bbbe4151fe66641fa539e119a2f
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-06-17 03:38:05 -0500

    map works, list work, auth works

--- resources/users.py
+++ resources/users.py
@@ -5,99 +5,132 @@ from flask_login import login_user, logout_user, login_required, current_user
 import models
 
 user_fields = {
-	'userName': fields.String,
-	'password': fields.String,
-	'email': fields.String
+    'userName': fields.String,
+    'password': fields.String,
+    'email': fields.String
 }
 
 class UserList(Resource):
-	def __init__(self):
-		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument(
-			'userName',
-			required=True,
-			help='No username provided',
-			location=['form', 'json']
-		)
-		self.reqparse.add_argument(
-			'password',
-			required=True,
-			help='No password provided',
-			location=['form', 'json']
-		)
-		self.reqparse.add_argument(
-			'email',
-			required=True,
-			help='No email provided',
-			location=['form', 'json']
-		)
-		super().__init__()
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'userName',
+            required=True,
+            help='No username provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'password',
+            required=True,
+            help='No password provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'email',
+            required=True,
+            help='No email provided',
+            location=['form', 'json']
+        )
+        super().__init__()
 
-	@marshal_with(user_fields)
-	def post(self):
-		args = self.reqparse.parse_args()
-		print((args), 'arguments to form body')
-		if args['password'] == args['password']:
-			print(args, '<==== arguments to form body')
-			user = models.User.create(**args)
-			login_user(user)
-			return marshal(user, user_fields)
-			# return marshal(user, user_fields), make_response(
-			# 	json.dumps({
-			# 		'success': True
-			# 		}), 200)
-		return make_response(
-			json.dumps({
-				'error': 'username taken '
-				}), 400)
+    def post(self):
+        args = self.reqparse.parse_args()
+        print((args), 'arguments to form body')
+        if args['password'] == args['password']:
+            print(args, '<==== arguments to form body')
+            user = models.User.create_users(**args)
+            login_user(user)
+            return ({
+            	'data': user,
+            	'registered': True
+            	})      
+
+class GetUserData(Resource):
+    def get(self):
+        return('Restaurants')
 
 class User(Resource):
-	def __init__(self):
-		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument(
-			'userName',
-			required=True,
-			help='No username provided',
-			location=['form', 'json']
-		)
-		self.reqparse.add_argument(
-			'password',
-			required=True,
-			help='No password provided',
-			location=['form', 'json']
-		)
-		self.reqparse.add_argument(
-			'email',
-			required=False,
-			help='No email provided',
-			location=['form', 'json']
-		)
-		super().__init__()
-
-	@marshal_with(user_fields)
-	def post(self):
-		args = self.reqparse.parse_args()
-		try:
-			logged_in_user = models.User.get(models.User.userName==args.userName)
-			print(logged_in_user)
-		except models.User.DoesNotExist:
-			abort(404)
-		else:
-			return (logged_in_user, 201)
-
-	@marshal_with(user_fields)
-	def get(self):
-		try:
-			print(current_user,'<----- CURRENT USER ')
-			logged_out_user=current_user
-			logout_user()
-		except models.User.DoesNotExist:
-			abort(404)
-		else:
-			return (logged_out_user, 201)
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'userName',
+            required=True,
+            help='No username provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'password',
+            required=True,
+            help='No password provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'email',
+            required=False,
+            help='No email provided',
+            location=['form', 'json']
+        )
+        super().__init__()
 
 
+    def post(self):
+        args = self.reqparse.parse_args()
+        try:
+            logged_in_user = models.User.get(models.User.userName==args.userName)
+            print(logged_in_user.userName)
+        except models.User.DoesNotExist:
+            abort(404)
+        else:
+            user = marshal(logged_in_user, user_fields)
+            return ({
+                    'data': user,
+                    'success': True
+                    })
 
+    def get(self):
+        try:
+            print(current_user,'<----- CURRENT USER ')
+            logged_out_user=current_user
+            logout_user()
+        except models.User.DoesNotExist:
+            abort(404)
+        else:
+            return ({
+            	'data': current_user,
+            	'loggedout': True
+            	})
+
+
+# ///this route returns all comments made by a user's session///
+# router.get('/usercomments', async (req, res, next) => {
+#     try{
+#         if (req.session.logged === true){
+
+#             const foundUser = await User.findOne({userName: req.session.userName})
+#             if (foundUser){
+#                 const foundRestaurants = await Restaurant.find({userName: req.session.userName}).populate('comments');
+#                 const foundComments = await Comment.find({commentAuthor: req.session.userName})
+#                 res.json({
+#                     status: 200,
+#                     data: foundRestaurants, foundComments
+#                 });
+#             } else {
+#                 res.json({
+#                     status: 400,
+#                     data: 'no data found'
+#                 });
+#             }
+#         } else {
+#             res.json({
+#                 status: 400,
+#                 data: 'no user session found'
+#             })
+#         }
+#     }catch(err){
+#         console.error(err);
+#         next(err);
+#     }
+# });
 
 
 users_api = Blueprint('resources.user', __name__)
@@ -105,17 +138,22 @@ users_api = Blueprint('resources.user', __name__)
 api = Api(users_api)
 
 api.add_resource(
-	UserList,
-	'/register/'
+    UserList,
+    '/register/'
 )
 
 api.add_resource(
-	User,
-	'/logout/'
+    User,
+    '/logout/'
 )
 
 api.add_resource(
-	User,
-	'/login/',
-	endpoint='auth'
+    GetUserData,
+    '/usercomments/'
+)
+
+api.add_resource(
+    User,
+    '/login/',
+    endpoint='auth'
 )
--- servercommands.py
+++ servercommands.py
@@ -3,3 +3,33 @@
 # source .env/bin/activate
 
 # pip3 install flask-restful peewee flask psycopg2 flask_login flask_cors
+
+# GetUserComents class
+# class GetUserComments(Resource):
+# 	def __init__(self):
+# 		self.reqparse = reqparse.RequestParser()
+# 		self.reqparse.add_argument(
+# 			'commentAuthor',
+# 			required=True,
+# 			help='no comment author provided',
+# 			location='form'
+# 		)
+# 		self.reqparse.add_argument(
+# 			'commentBody',
+# 			required=True,
+# 			help='no comment body provided',
+# 			location='json'
+# 		)
+# 		self.reqparse.add_argument(
+# 			'restaurant_name',
+# 			required=True,
+# 			help='no restaurant name provided',
+# 			location='json'
+# 		)
+# 		self.reqparse.add_argument(
+# 			'restaurant_id',
+# 			required=True,
+# 			help='no restaurant id provided',
+# 			location='json'
+# 		)
+# 		super().__init__()
